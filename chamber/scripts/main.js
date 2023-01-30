7bb7933007e410d744c103e181f308720730ab85
ReArquitecto <94197258+ReArquitecto@users.noreply.github.com>
2023-01-30 14:00:03 -0700

    cool

---
+++ chamber/scripts/main.js
@@ -0,0 +1,17 @@
+//get current year
+let date = new Date();
+let year = date.getFullYear();
+
+
+console.log(year);
+console.log(date);
+
+
+document.querySelector('.copyright-year').innerHTML = '&copy;' + year;
+
+let currentdate = document.lastModified;
+
+document.querySelector('.last-updated').innerHTML = 'Last Updated on ' + currentdate;
+
+
+document.querySelector('.current-date').innerHTML = year;
