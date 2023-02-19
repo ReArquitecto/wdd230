7b662a2129786a24fd45acca55ab9d402800af94
ReArquitecto <94197258+ReArquitecto@users.noreply.github.com>
2023-02-19 00:56:07 -0700

    pictures

---
+++ lesson07/scripts/lesson07.js
@@ -0,0 +1,25 @@
+// Select all images with the class "lazy"
+const images = document.querySelectorAll("img");
+
+// Define the options for the IntersectionObserver
+const options = {
+  rootMargin: "0px",
+  threshold: 0.5
+};
+
+// Create the IntersectionObserver object
+const observer = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(function(entry) {
+    if (entry.isIntersecting) {
+      // Replace the placeholder image with the real image
+      entry.target.setAttribute("src", entry.target.getAttribute("data-src"));
+      // Stop observing the image
+      observer.unobserve(entry.target);
+    }
+  });
+}, options);
+
+// Loop through each image and observe it
+images.forEach(function(image) {
+  observer.observe(image);
+});
