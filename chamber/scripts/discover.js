347433eeb61cf97382e071bec631547a53851367
ReArquitecto <94197258+ReArquitecto@users.noreply.github.com>
2023-02-24 18:53:25 -0700

    almost

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,26 @@
+// Select all images with the class "lazy"
+const images = document.querySelectorAll("img.lazy");
+
+// Define the options for the IntersectionObserver
+const options = {
+  rootMargin: "0px",
+  threshold: 0.35
+};
+
+// Create the IntersectionObserver object
+const observer = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(function(entry) {
+    if (entry.isIntersecting) {
+      // Replace the placeholder image with the real image
+      entry.target.setAttribute("src", entry.target.getAttribute("data-src"));
+      entry.target.classList.add("lazy-loaded");
+      // Stop observing the image
+      observer.unobserve(entry.target);
+    }
+  });
+}, options);
+
+// Loop through each image and observe it
+images.forEach(function(image) {
+  observer.observe(image);
+});
