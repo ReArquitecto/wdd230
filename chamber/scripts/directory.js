bae9c1f18ba80fb8473c42de0c47a6cc7bd4ced1
ReArquitecto <94197258+ReArquitecto@users.noreply.github.com>
2023-03-10 15:37:50 -0700

    update json

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -17,26 +17,24 @@ function displayCompanies(companies) {
       card.classList.add("card");
       let h2 = document.createElement('h2');
       let email = document.createElement('p');
-      let phone = document.createElement('p');
+      let website = document.createElement('p');
       let address = document.createElement('p');
   
       // Build the h2 content out to show the p full name - finish the template string
-      h2.textContent = `${company.name}`;
-      email.textContent = `${company.email}`;
-      phone.textContent = `Phone: ${company.phone}`;
-      address.textContent = `Address: ${company.address}`;
+      h2.textContent = company.name;
+      email.textContent = company.email;
+      website.textContent = company.website;
+      address.textContent = company.address;
   
       // Build the image portrait by setting all the relevant attribute
-      logo.setAttribute('src', company.logo);
+      logo.setAttribute('src', company.image);
       logo.setAttribute('alt', `Logo of ${company.name}`);
       logo.setAttribute('loading', 'lazy');
-      logo.setAttribute('width', '340');
-      logo.setAttribute('height', '440');
   
       // Append the section(card) with the created elements
       card.appendChild(logo);
       card.appendChild(h2);
-      card.appendChild(phone);
+      card.appendChild(website);
       card.appendChild(address);
   
       cards.appendChild(card);
