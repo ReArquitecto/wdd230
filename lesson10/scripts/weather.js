1e75dd05232e21ae617b9148da6efd8a52d0a507
ReArquitecto <94197258+ReArquitecto@users.noreply.github.com>
2023-03-10 20:59:43 -0700

    weather

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -3,5 +3,15 @@ const getWeather = async () => {
     const response = await fetch(apiURL);
     const data = await response.json();
     console.log(data);
+    let weather_icon_source = 'https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/1st%20Set%20-%20Color/' + data.currentConditions.icon + '.svg'
+    let weather_icon_description = data.currentConditions.icon;
+    document.querySelector('#weather_icon').setAttribute('src', weather_icon_source);
+    document.querySelector('#weather_icon').setAttribute('alt', weather_icon_description);
+    let t = data.currentConditions.temp;
+    document.querySelector('#t').textContent = Math.round(t * 5/9 +32 );
+    document.querySelector('#condition').textContent =  data.currentConditions.conditions;
+    document.querySelector('#ws').textContent = data.currentConditions.windspeed;
+
+
   };
 getWeather();
